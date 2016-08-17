ea354884a1c977426b5453380dddcf489261aff4
Nick Mak <nickmak@lanecrawford.com>
2016-08-17 11:45:56 +0800

    add template project

---
+++ CI TemplateTests/CI_TemplateTests.swift
@@ -0,0 +1,36 @@
+//
+//  CI_TemplateTests.swift
+//  CI TemplateTests
+//
+//  Created by user on 17/8/2016.
+//  Copyright © 2016 Kcinapp. All rights reserved.
+//
+
+import XCTest
+@testable import CI_Template
+
+class CI_TemplateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
